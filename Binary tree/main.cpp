b549b5399875e1c9a74a16f7a608b6e4718a3f8f
eldardamirov <eldardamirov75@mail.ru>
2018-04-08 18:45:29 +0300

    Initial Commit

---
+++ Binary tree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Binary tree
+//
+//  Created by Эльдар Дамиров on 08.04.2018.
+//  Copyright © 2018 Эльдар Дамиров. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
